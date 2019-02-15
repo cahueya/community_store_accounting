0ba652afe99e133c999ef49022794779fc6141d5
cahueya <cahueya@users.noreply.github.com>
2019-02-15 21:37:55 +0100

    Update accounting_job.php

--- jobs/accounting_job.php
+++ jobs/accounting_job.php
@@ -1,11 +1,10 @@
 <?php
 namespace Concrete\Package\CommunityStoreAccounting\Job;
+
 use Concrete\Core\Http\ServerInterface;
 use Concrete\Core\Package\Package;
 use Database;
 use Core;
-use Log;
-use Whoops\Exception\ErrorException;
 use Events;
 use Job;
 use Loader;
@@ -15,13 +14,9 @@ use \Concrete\Package\CommunityStore\Src\CommunityStore\Order\OrderItem as Store
 use \Concrete\Package\CommunityStore\Src\CommunityStore\Product\Product as StoreProduct;
 use Concrete\Package\CommunityStore\Src\CommunityStore\Order\Order as StoreOrder;
 use \Concrete\Package\CommunityStore\Src\CommunityStore\Utilities\Price as Price;
-
 use \Concrete\Package\CommunityStore\Src\Attribute\Key\StoreOrderKey as StoreOrderKey;
 use Concrete\Package\CommunityStore\Src\CommunityStore\Payment\Method as StorePaymentMethod;
 use Concrete\Package\CommunityStore\Src\Attribute\Value\StoreOrderValue as StoreOrderValue;
-
-
-
 use Concrete\Package\CommunityStoreAccounting\Src;
 
 final class AccountingJob extends Job
@@ -36,27 +31,12 @@ final class AccountingJob extends Job
     {
         return t("Submits Accounting Data to the Tax handler.");
     }
-
+    
     public function run() 
     {        
-  
-        $accounting = \Concrete\Package\CommunityStoreAccounting\Src\AccountingProcess::run_job();
-        
-        
+        $accounting = \Concrete\Package\CommunityStoreAccounting\Src\AccountingProcess::run_job(); 
         return t(
             'The accounting submission was successfull.'
         );
-        
-        
-        	
     }
-}    
-    
-    
-    
-    
-
-
-
-
-    
+}
