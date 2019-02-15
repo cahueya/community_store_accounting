d7c0cf0cbaa6ccc722e14d84aec8696ef9629a5e
cahueya <cahueya@users.noreply.github.com>
2019-02-15 21:41:00 +0100

    Update accounting_history.php

--- controllers/single_page/dashboard/store/reports/accounting_history.php
+++ controllers/single_page/dashboard/store/reports/accounting_history.php
@@ -1,32 +1,12 @@
 <?php
-
-
 namespace Concrete\Package\CommunityStoreAccounting\Controller\SinglePage\Dashboard\Store\Reports;
+
 use \Concrete\Core\Page\Controller\DashboardPageController;
 use Concrete\Core\Package\Package;
 use Database;
 use Core;
-use Log;
-use Whoops\Exception\ErrorException;
 use Loader;
-use Config;
-use Concrete\Package\CommunityStore\Src\CommunityStore\Order;
-use \Concrete\Package\CommunityStore\Src\CommunityStore\Utilities\Price as Price;
-use Concrete\Package\CommunityStore\Src\CommunityStore\Order\Order as StoreOrder;
-use \Concrete\Package\CommunityStore\Src\Attribute\Key\StoreOrderKey as StoreOrderKey;
-use \Concrete\Package\CommunityStore\Src\CommunityStore\Product\Product as StoreProduct;
-use \Concrete\Package\CommunityStore\Src\CommunityStore\Order\OrderList as StoreOrderList;
-use \Concrete\Package\CommunityStore\Src\CommunityStore\Order\OrderItem as StoreOrderItem;
-use Concrete\Package\CommunityStore\Src\Attribute\Value\StoreOrderValue as StoreOrderValue;
-use Concrete\Package\CommunityStore\Src\CommunityStore\Payment\Method as StorePaymentMethod;
-
 use Job;
-
-
-
-
-
-
 
 
 defined('C5_EXECUTE') or die("Access Denied.");
@@ -42,11 +22,7 @@ class AccountingHistory extends DashboardPageController
     public function run_job() {  
         $sending_job = Job::getByHandle('accounting_job');
 		$sending_job->executeJob();
-        $this->set('message', t("Job done"));
-        
+        $this->set('message', t("Job done"));        
     }
-
-    
-
 }
 ?>
